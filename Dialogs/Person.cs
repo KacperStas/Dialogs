6112a9f567dab9d4431ca6f6d26e67538562d5d3
Kacper Stasiak <stasiak24166@stud.akademiabialska.pl>
2026-05-13 01:04:07 +0200

    Dodaj pliki projektów.

---
+++ Dialogs/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonManager
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public string City { get; set; }
+
+        public Person(string firstName, string lastName, int birthYear, string city)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+            City = city;
+        }
+
+        // Required by Point 1: Determines how the object is displayed in the ListBox
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} (born: {BirthYear}), {City}";
+        }
+    }
+}
