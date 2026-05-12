24bfdc3a10a9e5400cc74f9f3ca4589b24b4843a
Kacper Stasiak <stasiak24166@stud.akademiabialska.pl>
2026-05-13 01:05:59 +0200

    Setup Main Window Layout and Data Binding

--- Dialogs/MainWindow.xaml.cs
+++ Dialogs/MainWindow.xaml.cs
@@ -1,24 +1,28 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Dialogs
+namespace PersonManager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // ObservableCollection automatically notifies the ListBox of additions
+        public ObservableCollection<Person> PeopleList { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            PeopleList = new ObservableCollection<Person>();
+            PeopleListBox.ItemsSource = PeopleList;
+        }
+
+        private void AddMultipleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Placeholder for Commit 3
+        }
+
+        private void AddSingleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Placeholder for Commit 4
         }
     }
 }
